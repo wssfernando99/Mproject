532c8adf3e2410338924f1e2b8058525343845c3
sachin20731 <munasinghe20731@gmail.com>
2023-11-07 02:07:18 +0530

    modify app.js file and route pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
-import './App.css';
-import { Welcome } from './pages/Welcome';
+import React from "react";
+// import Register from './component/Reg';
+import  Login from "./pages/Login";
+import Register from "./pages/Register";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <div className='App'>
-      <Welcome />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/home" element={<Homepage />} />
+      </Routes>
+    </Router>
   );
 }
 
