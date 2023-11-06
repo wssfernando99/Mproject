0569b755147c0c7ac640f1ba5c2ef5ca51c69775
wssfernando99 <sarindusauranga@gmail.com>
2023-11-07 03:40:35 +0530

    route pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 
-import './App.css';
-import Navbar from './pages/components/Navbar';
 import { Welcome } from './pages/Welcome';
 import { TestPage } from './pages/TestPage';
-
-
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Homepage } from './pages/Homepage';
 
 function App() {
   return (
-
-    <div>
-      <Welcome />
-    </div>
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route exact path='/' element={<Welcome />} />
+          <Route path='/Homepage' element={<Homepage />} />
+          <Route path='/TestPage' element={<TestPage />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+    
     
   );
 }
