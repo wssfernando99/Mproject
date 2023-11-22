c3c4045d979ebfe66fd20c4e818e1569540715b1
wssfernando99 <sarindusauranga@gmail.com>
2023-11-23 00:37:33 +0530

    Protect pages
    create function to protect pages according to login and logout

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import { Homepage } from './pages/Homepage';
 import { Aboutpage } from "./pages/Aboutpage";
+import { Protectedlogout, Protectedlogin } from "./pages/components/Protected";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route exact path='/' element={<Welcome />} />
-        <Route path='/Homepage' element={<Homepage />} />
-        <Route path='/TestPage' element={<TestPage />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
+      <Route exact path='/' element={<Protectedlogin><Welcome /></Protectedlogin>} />
+        <Route path='/Homepage' element={<Protectedlogout><Homepage /></Protectedlogout>} />
+        <Route path='/TestPage' element={<Protectedlogout><TestPage /></Protectedlogout>} />
+        <Route exact path="/register" element={<Protectedlogin><Register /></Protectedlogin>} />
+        <Route exact path="/login" element={<Protectedlogin><Login /></Protectedlogin>} />
         <Route exact path="/aboutpage" element={<Aboutpage />} />
       </Routes>
     </BrowserRouter>
