452e5992570823fcaf1b3e832655d8b81b2543e9
wssfernando99 <sarindusauranga@gmail.com>
2024-01-07 18:27:00 +0530

    update home page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -27,6 +27,38 @@ export const Homepage = () => {
             </div>
         </div>
        </div>
+       <div className='wrapper3'>
+        <div className='wrapper3topic'>
+          <h1>What is Autism?</h1>
+        </div>
+        <div className='wrapper3para'>
+          <p>There is no one type of autism, but many.</p>
+        </div>
+       </div>
+       <div className='wrapper4'>
+        <div className='wrapper4para'>
+          <p>Autism, or autism spectrum disorder (ASD), refers to a broad range of conditions characterized by challenges with social skills, repetitive behaviors, speech and nonverbal communication. According to the Centers for Disease Control, autism affects an estimated 1 in 36 children in the United States today.
+          </p><br/>
+          <p>
+          We know that there is not one autism but many subtypes, most influenced by a combination of genetic and environmental factors. Because autism is a spectrum disorder, each person with autism has a distinct set of strengths and challenges. The ways in which people with autism learn, think and problem-solve can range from highly skilled to severely challenged. Some people with ASD may require significant support in their daily lives, while others may need less support and, in some cases, live entirely independently.
+          </p>
+        </div>
+
+       </div>
+       <div className='wrapper5'>
+        <div className='wrapper5logo'>
+        </div>
+        <div className='wrapper5para'>
+        <p>
+        If you've met one person with autism, you've met one person with autism.
+        </p>
+        </div>
+        <div className='wrapper5para1'>
+        <p>
+        - DR. STEPHEN SHORE
+        </p>
+        </div>
+       </div>
     </div>
     
     
